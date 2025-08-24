e893028b8e10cb47a2b70a503e04b44b0b5a9c89
Bruno Assis <143642369+Brun04ss1s@users.noreply.github.com>
2025-08-24 20:09:30 -0300

    Ajustando o angular.json e o pollyfils

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { PostoListComponent } from './components/posto-list/posto-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, PostoListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('abastece-salvador-app');
+  title = 'abastecerSalvadorApp';
 }
